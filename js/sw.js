73d4b4ffb4094e303c41bed5fb28e63043a4e205
VincentMichuki <vincentmichuki@gmail.com>
2017-09-27 13:55:40 +0300

    'm'

---
+++ js/sw.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Jose Kim on 9/27/2017.
+ */
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/css/app.css',
+    '/css/app.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            }, function (err) {
+                console.log('Failed to install coz', err)
+            })
+    );
+});
